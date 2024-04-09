b9d0b256722eda47843b66b963117c654ce6abe0
Marcella Bortoletto Aleo <cella.aleo@gmail.com>
2024-04-09 19:08:26 +0100

    internacionalização da aplicação e uso do método .errors.full_messages

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -12,11 +12,10 @@ class WarehousesController < ApplicationController
     #aqui dentro que iremos:
     # 1- Receber os dados enviados
     # 2- Criar um novo galpão no banco de dados
-      #Parameters: {"authenticity_token"=>"[FILTERED]", "warehouse"=>{"name"=>"Rio de Janeiro", "description"=>"galpão do RIO", "code"=>"RIO", "address"=>"Av Museu, 1000", "city"=>"Rio de Janeiro", "cep"=>"20100-000", "area"=>"1000"}, "commit"=>"Enviar"}
     warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area) #strong parameters => parâmetros fortes/seguros
     @warehouse = Warehouse.new(warehouse_params)
     
-    if @warehouse.save
+    if @warehouse.save  # => executa o .valid? => preenche o array de erros
       redirect_to root_path, notice: 'Galpão cadastrado com sucesso'
     else
       flash.now[:notice] = 'Galpão não cadastrado'
