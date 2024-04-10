e91d9e5d67a48e071dfec5989d62b574132763de
Marcella Bortoletto Aleo <cella.aleo@gmail.com>
2024-04-10 13:22:43 +0100

    organizamos o codigo com declaração de métodos private, before_action e uso de _partial

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,18 +1,13 @@
 class WarehousesController < ApplicationController
-  def show
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
-  end
+  before_action :set_warehouse, only: [:show, :edit, :update]
+
+  def show; end #usando ; para deixar método vazio em uma linha só
 
   def new
     @warehouse = Warehouse.new
   end
 
   def create
-    #aqui dentro que iremos:
-    # 1- Receber os dados enviados
-    # 2- Criar um novo galpão no banco de dados
-    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area) #strong parameters => parâmetros fortes/seguros
     @warehouse = Warehouse.new(warehouse_params)
     
     if @warehouse.save  # => executa o .valid? => preenche o array de erros
@@ -21,22 +16,11 @@ class WarehousesController < ApplicationController
       flash.now[:notice] = 'Galpão não cadastrado'
       render 'new'
     end
-    
   end
 
-  def edit
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
-  end
+  def edit; end
 
   def update
-    #Capybara::ElementNotFound:
-    #Unable to find xpath "/html" erro 204
-    @warehouse = Warehouse.find(params[:id])
-    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area)
-    
-    
-
     if @warehouse.update(warehouse_params)
       redirect_to warehouse_path(@warehouse.id), notice: 'Galpão atualizado com sucesso'
     else
@@ -44,4 +28,26 @@ class WarehousesController < ApplicationController
       render 'edit'
     end
   end
+
+  private 
+  def set_warehouse
+    @warehouse = Warehouse.find(params[:id])
+  end
+  
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area) 
+    #strong parameters => parâmetros fortes/seguros
+  end
 end
+
+=begin
+  Os strong params permite filtrar as informações enviadas pelo usuários
+pois é possível que modifiquem o formulário pela inspeção da página.
+  Assim, devemos declarar os params ANTES de fazer o .save ou o .update
+para que o Rails filtre e exclua dados não permitidos.
+  O usuário pode querer burlar, se cadastrando como admin, pode tentar
+colocar um desconto não válido etc.
+  Os params do create e do update podem ser diferentes (se quiser impedir, 
+por exemplo de se alterar o CPF depois do cadastro). Então reaproveitá-lo
+pode não ser uma boa prática
+=end
