21d1aaaf64285b0ffda7c8f395887a6c11d89a97
Marcella Bortoletto Aleo <cella.aleo@gmail.com>
2024-04-11 20:11:01 +0100

    cadastro de produto usando input collection_select

--- app/controllers/product_models_controller.rb
+++ app/controllers/product_models_controller.rb
@@ -1,5 +1,31 @@
 class ProductModelsController < ApplicationController
+  before_action :set_product_model, only: [:show]
   def index
     @product_models = ProductModel.all
+  end
+
+  def new 
+    @product_model = ProductModel.new
+    @suppliers = Supplier.all
+  end
+
+  def create
+    @product_model = ProductModel.new(product_model_params)
+
+    @product_model.save
+    redirect_to @product_model, notice: 'Modelo de produto cadastrado com sucesso'
+  end
+
+  def show
+  end
+
+  private
+  def set_product_model
+    @product_model = ProductModel.find(params[:id])
+  end
+
+  def product_model_params
+    params.require(:product_model).permit(:name, :weigth, :width, :heigth, 
+                                          :depth, :sku, :supplier_id)
   end
 end
---
+++ spec/system/product_models/user_register_product_model_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'Usuário cadastra um modelo de produto' do
+  it 'com sucesso' do
+    # Arrange: criar um fornecedor
+    supplier = Supplier.create!(corporate_name: 'Samsung Ltda', brand_name: 'Samsung', 
+                                registration_number: '34333444/3000-40',
+                                full_address: 'Av. Nações Unidas, 343', city: 'São Paulo', 
+                                state: 'SP', email: 'contato@samsung.com.br')
+    # Act
+    visit root_path
+    click_on 'Modelos de produtos'
+    click_on 'Cadastrar produto'
+    fill_in 'Nome', with: 'TV 40 Polegadas'
+    fill_in 'Peso', with: 10_000
+    fill_in 'Altura', with: 60
+    fill_in 'Largura', with: 90
+    fill_in 'Profundidade', with: 10
+    fill_in 'SKU', with: 'TV40-SAMS-XPTO'
+    select 'Samsung', from: 'Fornecedor'
+    click_on 'Enviar'
+
+    # Assert
+    expect(page).to have_content('Modelo de produto cadastrado com sucesso')
+    expect(page).to have_content('TV 40 Polegadas')
+    expect(page).to have_content('Fornecedor: Samsung')
+    expect(page).to have_content('SKU: TV40-SAMS-XPTO')
+    expect(page).to have_content('Dimensões: 60cm x 90cm x 10cm')
+    expect(page).to have_content('Peso: 10000g')
+  end
+end
